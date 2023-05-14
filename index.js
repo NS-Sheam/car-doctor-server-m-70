4b788ab0a8dc4398dd694671a5b629c5ad9c16f6
NS-Sheam <123sheamfeni@gmail.com>
2023-05-14 12:25:13 +0600

    jwt authorization done

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ const client = new MongoClient(uri, {
   }
 });
 
+// verify jwt token function 
+const verifyJWT = (req, res, next) =>{
+  console.log("hitting verify JWT");
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: "unauthorized access"})
+  }
+  const token = authorization.split(' ')[1];
+  console.log("token inside jwt", token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(403).send({error: true, message: "unauthorized access"})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -62,7 +81,13 @@ async function run() {
     })
 
     // booking rortes
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("came back after verify JWT", decoded);
+      if (decoded.email !== req.query.email) {
+        return res.send({error: 1, message: "forbidden access"})
+      }
+      // console.log(req.headers.authorization);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
